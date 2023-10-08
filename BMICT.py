88c3014d7389b63826c0c297ec02c2cda86ea119
TheKail <dinhno12313@gmail.com>
2023-10-08 20:06:28 +0700

    Update BMICT.py

--- BMICT.py
+++ BMICT.py
@@ -1,10 +1,14 @@
-from tkinter import* 
+from tkinter import *
 import sqlite3
-from math import*
+import hashlib
+import random
+import string
+from math import *
 import time
 import datetime
 from tkinter import messagebox
 from matplotlib import pyplot as plt
+
 BMIDB = sqlite3.connect("bmidatabase.db")
 c = BMIDB.cursor()
 class Welcome():
@@ -47,7 +51,7 @@ class bmicalculator():
         self.var = IntVar()
         self.frame = Frame(master,padx=10, pady=10, relief=RAISED, borderwidth=1)
         self.frame.pack(fill=BOTH, expand=True)
-        self.bmiage = Label(self.frame,text="Nhập tuổi(2 - 120)")
+        self.bmiage = Label(self.frame,text="Nhập tuổi")
         self.bmiage.grid(row=1, column=1)
         self.age = Entry(self.frame,textvariable=self.ageyear)
         self.age.grid(row=1, column=2, pady=5)
@@ -186,9 +190,286 @@ class chart():
         plt.ylabel("Cân nặng")
         plt.xlabel("Số lần tính")
         plt.show()
+
+class Register:
+    def __init__(self, master):
+        self.master = master
+        self.master.geometry('300x200+650+250')
+        self.master.title('Đăng ký')
+        self.master.configure(background='white')
+
+        self.label1 = Label(self.master, text="Tên tài khoản: ")
+        self.label1.grid(row=1, column=0, pady=5)
+        self.username = Entry(self.master)
+        self.username.grid(row=1, column=1, pady=5)
+
+        self.label2 = Label(self.master, text="Mật khẩu: ")
+        self.label2.grid(row=2, column=0, pady=5)
+        self.password = Entry(self.master, show="*")
+        self.password.grid(row=2, column=1, pady=5)
+        self.label3 = Label(self.master, text="Email: ")
+        self.label3.grid(row=3, column=0, pady=5)
+        self.email = Entry(self.master)
+        self.email.grid(row=3, column=1, pady=5)
+
+        self.register_button = Button(self.master, text="Đăng ký", command=self.register)
+        self.register_button.grid(row=4, columnspan=2, pady=10)
+
+        self.login_link = Label(self.master, text="Đã có tài khoản? Đăng nhập", fg='blue', cursor='hand2')
+        self.login_link.grid(row=5, columnspan=2, pady=5)
+        self.login_link.bind("<Button-1>", self.go_to_login)
+
+    def register(self):
+        username = self.username.get()
+        password = self.password.get()
+        email = self.email.get()
+
+        if len(username) < 6:
+            messagebox.showerror('Đăng ký', 'Tên tài khoản phải có ít nhất 6 ký tự.')
+            return
+
+        if len(password) < 8:
+            messagebox.showerror('Đăng ký', 'Mật khẩu phải có ít nhất 8 ký tự.')
+            return
+
+        # Kiểm tra tính hợp lệ của email
+        if not self.is_valid_email(email):
+            messagebox.showerror('Đăng ký', 'Email không hợp lệ.')
+            return
+
+        # Kiểm tra xem tài khoản đã tồn tại chưa
+        if self.is_username_exists(username):
+            messagebox.showerror('Đăng ký', 'Tên tài khoản đã tồn tại.')
+            return
+        
+        if self.is_email_exists(email):
+            messagebox.showerror('Đăng ký', 'Email đã tồn tại.')
+            return
+
+        # Lưu thông tin tài khoản vào CSDL (trong trường hợp thực tế, bạn cần mã hóa mật khẩu trước khi lưu)
+        c.execute("INSERT INTO Users (username, password, email) VALUES (?, ?, ?)", (username, hashlib.md5(password.encode()).hexdigest(), email))
+        BMIDB.commit()
+
+        messagebox.showinfo('Đăng ký', 'Đăng ký thành công!')
+        self.go_to_login()
+
+    def is_valid_email(self, email):
+        # Đây chỉ là một kiểm tra đơn giản, bạn có thể sử dụng biểu thức chính quy mạnh hơn để kiểm tra email.
+        if "@" in email and "." in email:
+            return True
+        return False
+
+    def is_username_exists(self, username):
+        c.execute("SELECT * FROM Users WHERE username=?", (username,))
+        user = c.fetchone()
+        if user:
+            return True
+        return False
+    
+    def is_email_exists(self, email):
+        c.execute("SELECT * FROM Users WHERE email=?", (email,))
+        user = c.fetchone()
+        if user:
+            return True
+        return False
+
+    def go_to_login(self, event=None):
+        self.master.destroy()
+        root = Tk()
+        myGUILogin = Login(root)
+        root.mainloop()
+
+
+class Login:
+    def __init__(self, master):
+        self.master = master
+        self.master.geometry('250x210+650+250')
+        self.master.title('Đăng nhập')
+        self.master.configure(background='white')
+
+        self.label1 = Label(self.master, text="Tên tài khoản: ")
+        self.label1.grid(row=0, column=0, pady=5)
+        self.username = Entry(self.master)
+        self.username.grid(row=0, column=1, pady=5)
+
+        self.label2 = Label(self.master, text="Mật khẩu: ")
+        self.label2.grid(row=1, column=0, pady=5,  sticky="w")
+        self.password = Entry(self.master, show="*")
+        self.password.grid(row=1, column=1, pady=5)
+
+        self.login_button = Button(self.master, text="Đăng nhập", command=self.login)
+        self.login_button.grid(row=2, columnspan=2, pady=5)
+
+        self.register_link = Label(self.master, text="Chưa có tài khoản? Đăng ký", fg='blue', cursor='hand2')
+        self.register_link.grid(row=5, columnspan=2, pady=5)
+        self.register_link.bind("<Button-1>", self.go_to_register)
+
+        self.forgot_password_button = Button(self.master, text="Quên mật khẩu", command=self.go_to_forgot_password)
+        self.forgot_password_button.grid(row=4, columnspan=2, pady=5)
+
+        self.change_password_button = Button(self.master, text="Đổi mật khẩu", command=self.go_to_change_password)
+        self.change_password_button.grid(row=3, columnspan=2, pady=5)
+
+
+    def login(self):
+        global logged_in_user
+        username = self.username.get()
+        password = self.password.get()
+
+        # Kiểm tra xem tài khoản và mật khẩu có tồn tại trong CSDL hay không
+        c.execute("SELECT * FROM Users WHERE username=? AND password=?", (username, hashlib.md5(password.encode()).hexdigest()))
+        user = c.fetchone()
+        if user:
+            logged_in_user = username
+            self.master.destroy()
+            root = Tk()
+            myGUIWelcome = Welcome(root)
+            root.mainloop()
+        else:
+            messagebox.showerror('Đăng nhập', 'Tên tài khoản hoặc mật khẩu không đúng.')
+
+    def go_to_register(self, event=None):
+        self.master.destroy()
+        root = Tk()
+        myGUIRegister = Register(root)
+        root.mainloop()
+
+    def go_to_forgot_password(self):
+        self.master.destroy()
+        root = Tk()
+        myGUIForgotPassword = ForgotPassword(root)
+        root.mainloop()
+
+    def go_to_change_password(self):
+        self.master.destroy()
+        root = Tk()
+        myGUIForgotPassword = ChangePassword(root)
+        root.mainloop()
+
+
+
+class ForgotPassword:
+    def __init__(self, master):
+        self.master = master
+        self.master.geometry('300x150+650+250')
+        self.master.title('Quên mật khẩu')
+        self.master.configure(background='white')
+
+        self.label1 = Label(self.master, text="Tên tài khoản: ")
+        self.label1.grid(row=0, column=0, pady=5)
+        self.username = Entry(self.master)
+        self.username.grid(row=0, column=1, pady=5)
+
+        self.label2 = Label(self.master, text="Email: ")
+        self.label2.grid(row=1, column=0, pady=5)
+        self.email = Entry(self.master)
+        self.email.grid(row=1, column=1, pady=5)
+
+        self.reset_password_button = Button(self.master, text="Đặt lại mật khẩu", command=self.reset_password)
+        self.reset_password_button.grid(row=2, columnspan=2, pady=10)
+
+        self.login_link = Label(self.master, text="Đã nhớ mật khẩu? Đăng nhập", fg='blue', cursor='hand2')
+        self.login_link.grid(row=3, columnspan=2, pady=5)
+        self.login_link.bind("<Button-1>", self.go_to_login)
+
+    def reset_password(self):
+        username = self.username.get()
+        email = self.email.get()
+
+        # Kiểm tra xem tài khoản có tồn tại trong CSDL hay không
+        c.execute("SELECT * FROM Users WHERE username=? AND email=?", (username, email))
+        user = c.fetchone()
+        if user:
+            # Trong thực tế, bạn có thể thực hiện các bước khác để đặt lại mật khẩu, như gửi email xác minh và cung cấp một liên kết đặt lại mật khẩu.
+            # Ở đây, chúng ta chỉ đặt lại mật khẩu thành một mật khẩu mặc định (ví dụ: "password123")
+            new_password = "password123"
+            c.execute("UPDATE Users SET password=? WHERE username=?", (hashlib.md5(new_password.encode()).hexdigest(), username))
+            BMIDB.commit()
+            messagebox.showinfo('Quên mật khẩu', 'Mật khẩu đã được đặt lại thành công. Mật khẩu mới là "password123". Vui lòng đăng nhập và đổi mật khẩu sau khi đăng nhập.')
+        else:
+            messagebox.showerror('Quên mật khẩu', 'Tên tài khoản hoặc email không đúng.')
+
+    def go_to_login(self, event=None):
+        self.master.destroy()
+        root = Tk()
+        myGUILogin = Login(root)
+        root.mainloop()
+
+class ChangePassword:
+    def __init__(self, master):
+        self.master = master
+        self.master.geometry('300x220+650+250')
+        self.master.title('Đổi mật khẩu')
+        self.master.configure(background='white')
+
+        self.label1 = Label(self.master, text="Tên tài khoản: ")
+        self.label1.grid(row=0, column=0, pady=5)
+        self.username = Entry(self.master)
+        self.username.grid(row=0, column=1, pady=5)
+
+        self.label2 = Label(self.master, text="Email: ")
+        self.label2.grid(row=1, column=0, pady=5)
+        self.email = Entry(self.master)
+        self.email.grid(row=1, column=1, pady=5)
+
+        self.label3 = Label(self.master, text="Mật khẩu cũ: ")
+        self.label3.grid(row=2, column=0, pady=5)
+        self.password_old = Entry(self.master, show="*")
+        self.password_old.grid(row=2, column=1, pady=5)
+        
+        self.label4 = Label(self.master, text="Mật khẩu mới: ")
+        self.label4.grid(row=3, column=0, pady=5)
+        self.password_new = Entry(self.master, show="*")
+        self.password_new.grid(row=3, column=1, pady=5)
+
+        self.reset_password_button = Button(self.master, text="Đổi mật khẩu", command=self.change_password)
+        self.reset_password_button.grid(row=4, columnspan=2, pady=10)
+
+        self.login_link = Label(self.master, text="Đăng nhập", fg='blue', cursor='hand2')
+        self.login_link.grid(row=5, columnspan=2, pady=5)
+        self.login_link.bind("<Button-1>", self.go_to_login)
+
+    def change_password(self):
+        username = self.username.get()
+        email = self.email.get()
+        password_old = self.password_old.get()
+        password_new = self.password_new.get()
+        
+        # Kiểm tra xem tài khoản có tồn tại trong CSDL hay không
+        c.execute("SELECT * FROM Users WHERE username=? AND password=? AND email=?", (username, hashlib.md5(password_old.encode()).hexdigest(), email))
+        user = c.fetchone()
+        if user:
+            new_password = password_new
+            c.execute("UPDATE Users SET password=? WHERE username=?", (hashlib.md5(new_password.encode()).hexdigest(), username))
+            BMIDB.commit()
+            messagebox.showinfo('Quên mật khẩu', 'Mật khẩu đã được đổi thành công. Vui lòng đăng nhập và đổi mật khẩu sau khi đăng nhập.')
+        else:
+            messagebox.showerror('Quên mật khẩu', 'Thông tin tài khoản không đúng.')
+
+    def go_to_login(self, event=None):
+        self.master.destroy()
+        root = Tk()
+        myGUILogin = Login(root)
+        root.mainloop()
+
+
+
 def main():
-     root = Tk()
-     myGUIWelcome = Welcome(root)
-     root.mainloop()
-if __name__ == '__main__': 
-     main()
+    BMIDB = sqlite3.connect("bmidatabase.db")
+    c = BMIDB.cursor()
+
+    # Tạo bảng Users nếu nó chưa tồn tại
+    c.execute('''
+        CREATE TABLE IF NOT EXISTS Users (
+            id INTEGER PRIMARY KEY,
+            username TEXT,
+            password TEXT,
+            email TEXT
+        )
+    ''')
+    root = Tk()
+    myGUILogin = Login(root)  # Hiển thị giao diện đăng nhập
+    root.mainloop()
+
+if __name__ == '__main__':
+    main()
